3b9e5d58576cfb4a304c9493fc065a733949ff52
Nikita Kozhukhov <knik0405@gmail.com>
2023-08-31 13:29:17 +0300

    подключение JavaScript кода к блоку 1 (мульти-текст)

---
+++ scripts/mult-text.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".mult-text", {
+    strings: ["JavaScript-Разработчик", "Frontend-Разработчик", "Fullstack-разработчик"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+  });
